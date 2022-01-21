83867c36b7b7c13e4bc1c82a96f7e76673560d04
Vinicius Chagas <vmc.mart@gmail.com>
2022-01-20 21:58:09 -0300

    Client script para regra de somente números no CEP

---
+++ field_service_catalog_item/client_script_somente_num_cep.js
@@ -0,0 +1,15 @@
+/*
+Nome: "VC: Somente números "CEP""
+Type: onChange (cep)
+URL: https://dev96986.service-now.com/nav_to.do?uri=%2Fcatalog_script_client.do%3Fsys_id%3D119e83ab07c101101590f19d7c1ed068%26
+*/
+
+function onChange(control, oldValue, newValue, isLoading) {
+    if (isLoading || newValue == '') {
+		return;
+    }
+    if (!Number(newValue)) {
+		g_form.clearValue('cep');
+		g_form.showFieldMsg('cep', 'Por favor, insira apenas números no campo CEP.', 'error');
+    }
+}
