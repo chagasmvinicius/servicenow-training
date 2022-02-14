96d8b1e6b16c0e18d6fa10bbf927b188fb851cf9
Vinicius Chagas <vmc.mart@gmail.com>
2022-02-13 21:03:30 -0300

    Correção dos scripts do scheduled data import

--- exercicios/integracao_com_csv/scheduled_data_import_post_script_bool.js
+++ exercicios/integracao_com_csv/scheduled_data_import_post_script_bool.js
@@ -1,16 +1,20 @@
 /*
 Name: Run SFTP Flexera - Contratos (post_script_bool)
+Run as: flexera_integration
 */
 
-// Exclusão dos registros criados anteriormente na tabela ast_contract:
+try {
+    // Exclusão dos registros criados anteriormente na tabela ast_contract:
 
-var countDelete = 0;
-var contratos = new GlideRecord('ast_contract');
-contratos.addEncodedQuery('sys_created_onNOTONToday@javascript:gs.beginningOfToday()@javascript:gs.endOfToday()^sys_created_by=vimartin');
-contratos.query();
-while (contratosD.next()) {
-    contratos.deleteRecord();
-    countDelete++;
+    var countDelete = 0;
+    var contratos = new GlideRecord('ast_contract');
+    contratos.addEncodedQuery('sys_created_onNOTONToday@javascript:gs.beginningOfToday()@javascript:gs.endOfToday()^sys_created_by=flexera_integration');
+    contratos.query();
+    while (contratosD.next()) {
+        contratos.deleteRecord();
+        countDelete++;
+    }
+    gs.info('[Run SFTP Flexera - Contratos] Quantidade de registros deletados (são registros duplicados): ' + countDelete);
+} catch (e) {
+    gs.info('[Run SFTP Flexera - Contratos] Erro exclusão dos registros anteriores: ' + gs.getMessage(e));
 }
-
-gs.info('[Run SFTP Flexera - Contratos] Quantidade de registros deletados (são registros duplicados): ' + countDelete);
--- exercicios/integracao_com_csv/scheduled_data_import_pre_script_bool.js
+++ exercicios/integracao_com_csv/scheduled_data_import_pre_script_bool.js
@@ -1,24 +1,29 @@
 /*
 Name: Run SFTP Flexera - Contratos (pre_script_bool)
+Run as: flexera_integration
 */
 
-// Formatação do sufixo dos arquivos CSV:
+try {
+    // Formatação do sufixo dos arquivos CSV:
 
-var day = gs.nowDateTime().split('/')[0];
-var month = gs.nowDateTime().split('/')[1];
-var year = gs.nowDateTime().split('/')[2].split(' ')[0];
-var formattedDate = year.concat(month, day);
-var newFilePath = '/srtFtpData/JB-2K3-FTP-P01/flexera/10033_' + formattedDate.toString() + '.csv';
+    var day = gs.nowDateTime().split('/')[0];
+    var month = gs.nowDateTime().split('/')[1];
+    var year = gs.nowDateTime().split('/')[2].split(' ')[0];
+    var formattedDate = year.concat(month, day);
+    var newFilePath = '/srtFtpData/JB-2K3-FTP-P01/flexera/10033_' + formattedDate.toString() + '.csv';
 
-// Atualização do sufixo no Data Source "SFTP Flexera - Contratos":
+    // Atualização do sufixo no Data Source "SFTP Flexera - Contratos":
 
-var searchDataSource = new GlideRecord('sys_data_source');
-searchDataSource.addQuery('sys_id', 'dea7954107ad41101590f19d7c1ed0f7');
-searchDataSource.setLimit(1);
-searchDataSource.query();
-if (searchDataSource.next()) {
-    searchDataSource.setValue('file_path', newFilePath);
+    var searchDataSource = new GlideRecord('sys_data_source');
+    searchDataSource.addQuery('sys_id', 'dea7954107ad41101590f19d7c1ed0f7');
+    searchDataSource.setLimit(1);
+    searchDataSource.query();
+    if (searchDataSource.next()) {
+        searchDataSource.setValue('file_path', newFilePath);
+    }
+    searchDataSource.update();
+
+    gs.info('[Run SFTP Flexera - Contratos] Data source atualizado com o file path: ' + newFilePath);
+} catch (e) {
+    gs.info('[Run SFTP Flexera - Contratos] Erro data source: ' + gs.getMessage(e));
 }
-searchDataSource.update();
-
-gs.info('[Run SFTP Flexera - Contratos] Data source atualizado com o file path: ' + newFilePath);
